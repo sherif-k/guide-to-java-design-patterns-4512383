54738b981d8668dc37409d8d55264046be481ee0
sherif-k <sherif.kotb@hotmail.com>
2024-06-05 00:09:48 +0000

    solution for factory pattern exercise

--- src/main/java/com/example/VendingMachine.java
+++ src/main/java/com/example/VendingMachine.java
@@ -3,8 +3,14 @@ package com.example;
 public class VendingMachine {
 
     Snack getSnack(String snackType) {
-        return null;
-    }
+        if (snackType.equalsIgnoreCase("ChocolateBar")) {
+            return new ChocolateBar();
+        } else if (snackType.equalsIgnoreCase("Chips")) {
+            return new Chips();
+        } else {
+            return new Drink();
+        }
 
+    }
 
 }

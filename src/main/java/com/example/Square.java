a5145e7698b1b44cc65ea19e06c4bcd0be993399
sherif-k <sherif.kotb@hotmail.com>
2024-06-05 20:55:13 +0000

    solution to bridge exercise

--- src/main/java/com/example/Square.java
+++ src/main/java/com/example/Square.java
@@ -1,8 +1,20 @@
 package com.example;
 
 public class Square implements Shape {
+
+    private final Color color;
+
+    public Square(Color color) {
+        this.color = color;
+    }
+
     @Override
     public void getType() {
         System.out.println("Square");
+    }
+
+    @Override
+    public void getColor() {
+        color.get();
     }
 }

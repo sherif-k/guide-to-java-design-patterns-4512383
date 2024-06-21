997254b789b89558a00f9b5592abbc7e9d3f6172
sherif-k <sherif.kotb@hotmail.com>
2024-06-21 21:33:15 +0000

    solution to startegy exercise

--- src/main/java/com/example/CheckoutPage.java
+++ src/main/java/com/example/CheckoutPage.java
@@ -2,16 +2,8 @@ package com.example;
 
 public class CheckoutPage {
 
-    public void payWithCard() {
-        System.out.println("Payment made with card");
-    }
-
     public void pay(Payment payment) {
         payment.Pay();
-    }
-
-    public void payByBankTransfer() {
-        System.out.println("Payment made by bank transfer");
     }
 
 }

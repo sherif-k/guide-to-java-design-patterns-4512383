cb9477d91ab1135a51e1eb37144f79718cc11772
sherif-k <sherif.kotb@hotmail.com>
2024-06-22 20:59:42 +0000

    solution to template exercise

--- src/main/java/com/example/MeatFeastPizza.java
+++ src/main/java/com/example/MeatFeastPizza.java
@@ -1,18 +1,12 @@
 package com.example;
 
-public class MeatFeastPizza {
+public class MeatFeastPizza extends Pizza {
 
-    public void makeBase() {
-        System.out.println("Mix flour, yeast and salt.");
-        System.out.println("Roll out the dough.");
-    }
+
 
     public void addToppings() {
-        System.out.println("Add pepperoni, ham and chicken" );
+        System.out.println("Add pepperoni, ham and chicken");
     }
 
-    public void cook() {
-        System.out.println("Cook in the oven for 20 minutes");
-    }
 
 }
